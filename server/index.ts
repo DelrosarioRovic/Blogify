94883fee133d44c7bbfaf222e5b90373db170df4
DelrosarioRovic <rovicdelrosario01@gmail.com>
2023-04-11 17:50:12 +0800

    create new project dir

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express';
+import cors from "cors";
+
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  console.log(email,password)
+})
+
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
