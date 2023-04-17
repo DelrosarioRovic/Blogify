cc016b89b6247f06cb09e90e0c52231ab78232ff
DelrosarioRovic <rovicdelrosario01@gmail.com>
2023-04-17 18:04:01 +0800

    successfully applied github

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import express, { Request, Response, NextFunction } from "express";
 import passport from "passport";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
-import thirdPartyMwAuth from './middleware/thirdpartymwAuth';
+import thirdPartyMwAuth from "./middleware/thirdpartymwAuth";
 import bcrypt from "bcrypt";
 dotenv.config();
 
@@ -15,12 +15,12 @@ import { MiddlewareLocal, CustomRequest } from "./middleware/middlewareAuth";
 
 //mongo Db connection
 connectToDatabase()
-.then(() => {
-  console.log("MongoDB connected");
-})
-.catch((error) => {
-  console.error("MongoDB connection error:", error);
-});
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
 const app = express();
 //middleware
@@ -37,25 +37,56 @@ app.use(
 //third Party authentication middleware
 thirdPartyMwAuth();
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-app.get('/auth/google/callback', passport.authenticate('google', { session: false }), async(req:Request, res:Response) => {
-  const userToken = req.user as any;
-  const googleUser = {
-    googleId: userToken.googleId,
-    displayName: userToken.displayName,
-    email:userToken.email
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { session: false }),
+  async (req: Request, res: Response) => {
+    const userToken = req.user as any;
+    const googleUser = {
+      googleId: userToken.googleId,
+      displayName: userToken.displayName,
+      email: userToken.email,
+    };
+    const secret = process.env.userLocalSecret as string;
+    const token = jwt.sign(googleUser, secret, { expiresIn: "1h" });
+    res.cookie("access_token", token, {
+      httpOnly: true,
+    });
+    res.redirect("http://localhost:5173");
   }
-  const secret = process.env.userLocalSecret as string;
-  const token = jwt.sign(googleUser, secret, { expiresIn: "1h" });
-  res.cookie("access_token", token, {
-    httpOnly: true,
-  });
-  res.json({ message: "Logged in successfully", user: googleUser });
-});
+);
 
-app.get("/user", MiddlewareLocal,
+app.get("/auth/github", passport.authenticate("github"));
+
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { session: false }),
+  async (req: Request, res: Response) => {
+    const userToken = req.user as any;
+    const googleUser = {
+      googleId: userToken.googleId,
+      displayName: userToken.displayName,
+      email: userToken.email,
+    };
+    const secret = process.env.userLocalSecret as string;
+    const token = jwt.sign(googleUser, secret, { expiresIn: "1h" });
+    res.cookie("access_token", token, {
+      httpOnly: true,
+    });
+    res.redirect("http://localhost:5173");
+  }
+);
+
+app.get(
+  "/user",
+  MiddlewareLocal,
   async (req: CustomRequest, res: Response, next: NextFunction) => {
+    console.log(req.userId);
     const userId = req.userId;
     const googleUserId = req.googleUserId;
     if (userId) {
@@ -63,16 +94,25 @@ app.get("/user", MiddlewareLocal,
       res.json({ authenticated: true, user: localUser });
     }
     if (googleUserId) {
-      const googleUser = await User.findOne({googleId:googleUserId});
+      const googleUser = await User.findOne({ googleId: googleUserId });
       res.json({ authenticated: true, user: googleUser });
     }
   }
 );
 
+app.get("/sign-out", async (req: Request, res: Response) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).send("Signed out successfully.");
+  } catch (error) {
+    res.status(500).send("An error occurred while signing out.");
+  }
+});
+
 app.post("/login", async (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
   // check if user exists
-  const user = await User.findOne({ email:email });
+  const user = await User.findOne({ email: email });
   if (!user) {
     return res.status(401).json({ message: "Invalid email or password" });
   }
--- server/middleware/thirdpartymwAuth.ts
+++ server/middleware/thirdpartymwAuth.ts
@@ -3,40 +3,78 @@ import User from "../models/users.model";
 import jwt from "jsonwebtoken";
 import dotenv from 'dotenv';
 const GoogleStrategy = require("passport-google-oauth20").Strategy; 
+const GitHubStrategy = require("passport-github2").Strategy;
 dotenv.config();
 
+
+const createUser = async (id:any, displayName:any, email:any) => {
+  try {
+    let user = await User.findOne({ googleId: id });
+    if (!user) {
+      user = new User({
+        googleId: id,
+        displayName: displayName,
+        email: email,
+      });
+      await user.save();
+    }
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
 const thirdPartyMwAuth = () => {
-    passport.use(
-        new GoogleStrategy(
-          {
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/auth/google/callback',
-            scope: ['profile', 'email'],
-          },
-          async (accessToken:any, refreshToken:any, profile:any, done:any) => {
-            // Generate a JWT token using the user's Google profile
-            try {
-              let user = await User.findOne({ googleId: profile.id });
-              if (!user) {
-                user = new User({
-                  googleId: profile.id,
-                  displayName: profile.displayName,
-                  email: profile.emails[0].value,
-                });
-                await user.save();
-              }
-      
-              // Generate a JWT token
+  passport.use(
+      new GoogleStrategy(
+        {
+          clientID: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          callbackURL: '/auth/google/callback',
+          scope: ['profile', 'email'],
+        },
+        async (accessToken:any, refreshToken:any, profile:any, done:any) => {
+          try {
+            const user = await createUser(profile.id, profile.displayName, profile.emails[0].value);
             const token = jwt.sign({ googleId: user.googleId,displayName:user.displayName,email:user.email }, process.env.userLocalSecret as string);
             const decoded = jwt.decode(token);
             done(null, decoded);
-      
-            } catch (error) {
-              console.log(error)
-            }
+            
+          } catch (error) {
+            console.log(error)
           }
-        )
-      );
+        }
+      )
+    );
+//----------------------------------------------------------------------------------------------
+    passport.use(
+      new GitHubStrategy(
+        {
+          clientID: process.env.GITHUB_CLIENT_ID,
+          clientSecret: process.env.GITHUB_CLIENT_SECRET,
+          callbackURL: '/auth/github/callback',
+        },
+        async (accessToken:any, refreshToken:any, profile:any, done:any) => {
+          try {
+            const user = await createUser(profile.id, profile.displayName, profile.username);
+            const token = jwt.sign({ googleId: user.googleId, displayName: user.displayName, email: user.email }, process.env.userLocalSecret as string);
+            const decoded = jwt.decode(token);
+            console.log(decoded);
+            done(null, decoded);
+
+          } catch (error) {
+            console.log(error)
+          }
+        }
+      )
+    );
+
+
 }
+
+
+
+
+
 export default thirdPartyMwAuth;
