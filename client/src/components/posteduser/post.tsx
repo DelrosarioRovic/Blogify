75d3993a93278f5b41fbfbb2a9c2c2d21afae2af
Alexiess <alexissmanalastas@gmail.com>
2023-04-27 14:42:31 +0800

    update ui

--- client/src/components/posteduser/post.tsx
+++ client/src/components/posteduser/post.tsx
@@ -47,7 +47,7 @@ const Post = () => {
         <div key={item.post_id} className="max-w-2xl mx-auto mt-4 overflow-hidden sm:rounded-md sm:border max-sm:border-y border-gray-300 relative">
 
        <img src={``} alt={`post_cover`} className="bg-black w-full h-36"/>
-       
+
         <div
           className="bg-stone-100 bg-opacity-[.4] max-md:px-4 active:outline active:outline-[2px] active:outline-slate-700 flex flex-col gap-4 md:px-12 py-4"
         >
