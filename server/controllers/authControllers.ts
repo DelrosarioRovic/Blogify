26d1322ae6bd595825c2b76faf5bf80633e5306c
DelrosarioRovic <rovicdelrosario01@gmail.com>
2023-05-26 14:28:00 +0800

    fixing auth bugs

--- server/controllers/authControllers.ts
+++ server/controllers/authControllers.ts
@@ -98,7 +98,11 @@ router.post("/update-profile", async (req: Request, res: Response) => {
 
 router.get("/sign-out", async (req: Request, res: Response) => {
     try {
-        res.clearCookie("access_token");
+      res.clearCookie("access_token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
         res.status(200).send("Signed out successfully.");
     } catch (error) {
         res.status(500).send("An error occurred while signing out.");
