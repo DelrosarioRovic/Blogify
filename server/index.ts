159d083eb82cd2775ef88c86e6fb6efc06a7412b
DelrosarioRovic <rovicdelrosario01@gmail.com>
2023-05-27 16:28:28 +0800

    fixing bugs

--- server/index.ts
+++ server/index.ts
@@ -33,9 +33,9 @@ const app = express();
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  cors()
-);
+app.use(cors({
+  origin: 'https://blogify-phi.vercel.app'
+}));
 
 //third Party authentication middleware
 thirdPartyMwAuth();
